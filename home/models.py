daf6fbf349e547bf542470949609ff05f51eea10
Jharana <103406836+JharanaAdk@users.noreply.github.com>
2023-01-19 15:56:02 +0545

    index

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class demo(models.Model):
+    name = models.CharField(max_length=150)
+    age = models.IntegerField()
+    time = models.TimeField(auto_now_add=True)
+    date = models.DateField(auto_created=True)
+    description = models.TextField(max_length=500)
+    slug = models.SlugField()
+    number = models.PositiveIntegerField()
+    number2 = models.IntegerField()
+
+    def __str__(self):
+        return self.slug
+# user decription model
+#books
